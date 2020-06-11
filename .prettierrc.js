a1ec07b9628780ee51d621c562e405fbf04c925e
Alexander Böhm <tools@boehm.work>
2020-06-11 16:39:28 +0200

    Improve prettier abd general styling settings

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  overrides: [
+    {
+      files: "*.json",
+      options: {
+        printWidth: 80
+      }
+    }
+  ],
+  endOfLine: "lf",
+  printWidth: 120,
+  semi: false,
+  tabWidth: 2,
+  useTabs: false
+}
