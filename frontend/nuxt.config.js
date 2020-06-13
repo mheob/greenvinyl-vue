be648e5371f4a661b4652543e028862196222ff4
Alexander Böhm <tools@boehm.work>
2020-06-13 09:34:04 +0200

    Change the header title tag

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "CLASSEN Greenvinyl – Der erste Vinyl-Bodenbelag mit dem Blauen Engel",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
