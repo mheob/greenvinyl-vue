0dd95c4351acb2ea3a2020eef36ad4537f67f951
Alexander Böhm <tools@boehm.work>
2020-06-21 10:43:35 +0200

    Use the smaller components on the retailer page

--- pages/retailer/index.vue
+++ pages/retailer/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <article>
+  <div>
     <ContentHeader title="Händlersuche">
       Finden Sie einfach und schnell einen CLASSEN Greenvinyl-Partner in Ihrer Nähe und lassen Sie sich zu dem
       umfangreichen Produktsortiment beraten. Geben Sie dazu in das Suchfeld Ihren Wohnort oder die PLZ ein.
@@ -10,83 +10,54 @@
     <GMap
       ref="gMap"
       :cluster="{ options: { styles: clusterStyle } }"
-      :center="{ lat: locations[1].lat, lng: locations[1].lng }"
+      :center="{ lat: retailerList[0].lat, lng: retailerList[0].lng }"
       :options="{
         fullscreenControl: false,
         streetViewControl: false,
-        mapTypeControl: false,
+        mapTypeControl: true,
         zoomControl: true,
         gestureHandling: 'cooperative'
       }"
-      :zoom="10"
+      :zoom="11"
+      @click="currentLocation = {}"
     >
       <GMapMarker
-        v-for="(location, i) in locations"
-        :key="i"
-        :position="{ lat: location.lat, lng: location.lng }"
-        :options="{ icon: location === currentLocation ? pins.selected : pins.notSelected }"
-        @click="currentLocation = location"
+        v-for="retailer in retailerList"
+        :key="retailer.key"
+        :position="{ lat: retailer.lat, lng: retailer.lng }"
+        :options="{ icon: retailer === currentLocation ? pins.selected : pins.notSelected }"
+        @click="currentLocation = retailer"
       >
         <GMapInfoWindow>
-          <h4 class="text-lg font-bold text-green-500">{{ location.company }}</h4>
-          <div>
-            <div class="flex mt-4">
-              <div class="icon" aria-label="Adresse">
-                <HeroIcons :type="Icon.LOCATION_MARKER" width-class="w-5" aria-hidden="true" />
-              </div>
-              <div class="content">{{ location.street1 }}<br />{{ location.zip }} {{ location.city }}</div>
-            </div>
-            <div v-if="location.mail" class="flex mt-4">
-              <div class="icon" aria-label="E-Mail">
-                <HeroIcons :type="Icon.MAIL" width-class="w-5" aria-hidden="true" />
-              </div>
-              <div class="content">{{ location.mail }}</div>
-            </div>
-            <div v-if="location.tel" class="flex mt-4">
-              <div class="icon" aria-label="Telefon">
-                <HeroIcons :type="Icon.PHONE" width-class="w-5" aria-hidden="true" />
-              </div>
-              <div class="content">
-                <a class="underline" :href="'tel:' + location.tel.replace(/ /g, '')">{{ location.tel }}</a>
-              </div>
-            </div>
-            <div v-if="location.fax" class="flex mt-4">
-              <div class="icon" aria-label="Fax">
-                <HeroIcons :type="Icon.PRINTER" width-class="w-5" aria-hidden="true" />
-              </div>
-              <div class="content">{{ location.fax }}</div>
-            </div>
-            <div class="flex mt-4">
-              <div class="icon" aria-label="Fax"></div>
-              <div class="content">
-                <a
-                  class="underline"
-                  :href="'http://www.google.com/maps/place/' + location.lat + ',' + location.lng"
-                  target="_blank"
-                >
-                  Route auf Google Maps berechnen
-                </a>
-              </div>
-            </div>
-          </div>
+          <h4 class="text-lg font-bold text-green-500">{{ retailer.name }}</h4>
+          <TheRetailerGMapInfoCard :retailer="retailer" />
         </GMapInfoWindow>
       </GMapMarker>
     </GMap>
-  </article>
+
+    <section class="container grid gap-8 mx-auto mt-8 md:mt-12 md:grid-cols-2 lg:max-w-6xl xl:grid-cols-3 xl:max-w-8xl">
+      <RetailerCard v-for="retailer in retailerList" :key="retailer.key" :retailer="retailer" class="p-8 lg:p-16" />
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue"
 
 import ContentHeader from "@/components/UI/ContentHeader.vue"
-import TheRetailerSearchForm from "@/components/Page/Retailer/TheRetailerSearchForm.vue"
-import HeroIcons, { Icon } from "@/components/Icons/HeroIcons.vue"
+import RetailerCard from "@/components/Retailer/RetailerCard.vue"
+import TheRetailerGMapInfoCard from "@/components/Retailer/TheRetailerGMapInfoCard.vue"
+import TheRetailerSearchForm from "@/components/Retailer/TheRetailerSearchForm.vue"
+import { Retailer } from "@/models/locations"
+
+import dummyRetailer from "@/data/dummy_retailer.json"
 
 export default Vue.extend({
   components: {
     ContentHeader,
-    TheRetailerSearchForm,
-    HeroIcons
+    RetailerCard,
+    TheRetailerGMapInfoCard,
+    TheRetailerSearchForm
   },
 
   data() {
@@ -102,62 +73,9 @@ export default Vue.extend({
     }
 
     return {
-      Icon,
       currentLocation: {},
       locationsVisibleOnMap: "",
-      locations: [
-        {
-          lat: 50.4386,
-          lng: 7.49002,
-          company: "BAUHAUS Neuwied",
-          street1: "Allensteiner Straße 17",
-          zip: "56566",
-          city: "Neuwied",
-          tel: "+49 2631 8374 0",
-          fax: "+49 2631 8374 13"
-        },
-        {
-          lat: 50.39614,
-          lng: 7.5121,
-          company: "BAUHAUS Mülheim-Kärlich",
-          street1: "In der Puetzgewann 2",
-          zip: "56218",
-          city: "Mülheim-Kärlich",
-          tel: "+49 2630 9431 0",
-          fax: "+49 2630 9431 13"
-        },
-        {
-          lat: 50.39545,
-          lng: 7.51375,
-          company: "RuckZuck Mülheim-Kärlich",
-          street1: "Spitalsgraben 10",
-          zip: "56218",
-          city: "Mülheim-Kärlich",
-          mail: "rzMuelheim@ruckzuck.biz",
-          tel: "+49 2630 96247 90",
-          fax: "+49 2630 96247 99"
-        },
-        {
-          lat: 50.38997,
-          lng: 7.55597,
-          company: "Hornbach Koblenz",
-          street1: "August-Thyssen-Str. 10",
-          zip: "56070",
-          city: "Koblenz",
-          tel: "+49 261 98458 0",
-          fax: "+49 261 98458 480"
-        },
-        {
-          lat: 50.38314,
-          lng: 7.57355,
-          company: "BAUHAUS Koblenz",
-          street1: "Otto-Schönhagen-Straße 1",
-          zip: "56070",
-          city: "Koblenz",
-          tel: "+49 261 983499 0",
-          fax: "+49 261 98458 13"
-        }
-      ],
+      retailerList: dummyRetailer as Retailer[],
       pins: {
         selected: marker,
         notSelected: {
