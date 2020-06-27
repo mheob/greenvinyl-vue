fb4af0989dd963a404ae82b755dad0bb7601dcf3
Alexander Böhm <tools@boehm.work>
2020-06-27 21:42:01 +0200

    Outsource the product reservation form and add validation

--- client/components/Product/TheProductHeader.vue
+++ client/components/Product/TheProductHeader.vue
@@ -4,15 +4,15 @@
       <!-- eslint-disable max-len -->
       <img
         class="object-cover w-full h-full"
-        src="https://classen_integrate.aocluster.com/v2.0/render_img_scene.php?cnf=int&amp;sid=8060&amp;view=0&amp;onam=&amp;rot=0&amp;artnr=48317&amp;mid=&amp;ofit=&amp;overuse=&amp;overorder=&amp;w=1280&amp;h=720&amp;dbg=0"
-        alt="Traubeneiche …DYN…"
+        :src="`https://classen_integrate.aocluster.com/v2.0/render_img_scene.php?cnf=int&sid=8060&artnr=${product.sap}&w=1280&h=720`"
+        :alt="product.name"
       />
       <!-- eslint-enable max-len -->
     </aside>
 
     <article class="relative bg-green-100 lg:w-1/2">
       <div class="flex items-start justify-start h-full p-4 lg:p-16">
-        <div class="max-w-lg">
+        <div class="max-w-xl">
           <header>
             <h2 class="text-lg lg:text-xl">{{ product.collection | upperFirstChar }}</h2>
             <h1 class="mt-1 text-4xl leading-none text-green-500 lg:text-6xl">{{ product.name }}</h1>
@@ -22,51 +22,7 @@
             </p>
           </header>
 
-          <section class="mt-12">
-            <form class="flex flex-col md:flex-row">
-              <div class="flex flex-wrap w-full">
-                <!-- eslint-disable max-len -->
-                <div
-                  class="flex items-center justify-end w-1/2 whitespace-no-wrap border-4 border-r-2 border-green-500 xl:w-auto"
-                >
-                  <span class="w-20 px-0 text-xl text-right cursor-default">{{ product.basePriceGross }}</span>
-                  <span class="pl-2 pr-3 cursor-default">€/{{ product.basePriceUnit }}</span>
-                </div>
-                <!-- eslint-enable max-len -->
-
-                <!-- eslint-disable max-len -->
-                <div class="flex items-center w-1/2 bg-white border-4 border-l-2 border-green-500 xl:w-auto">
-                  <input
-                    class="w-full pl-2 pr-0 text-xl text-right border-none shadow-none form-input hover:shadow-none focus:shadow-none"
-                    type="number"
-                    min="0"
-                    max="999"
-                    autofocus
-                    value="…DYN…"
-                  />
-                  <span class="pl-2 pr-3">{{ product.packagingUnitUnit }}</span>
-                </div>
-                <!-- eslint-enable max-len -->
-
-                <!-- eslint-disable max-len -->
-                <AppButton
-                  class="w-full px-4 py-3 text-xl whitespace-no-wrap btn btn-button-dark form-item xl:w-auto"
-                  :brightness="Brightness.DARK"
-                  :variant="Variant.BUTTON"
-                  is-button
-                  no-transform
-                >
-                  Menge eingeben …DYN…
-                </AppButton>
-                <!-- eslint-enable max-len -->
-              </div>
-            </form>
-
-            <div class="mt-3 text-sm">
-              {{ product.packagingUnit }} à {{ pricePerPackagingUnit | digits(2) }} € (ca.
-              {{ product.packagingUnitContent }} {{ product.packagingUnitUnit }}) <span class="font-semibold"></span>
-            </div>
-          </section>
+          <TheProductReservation :product="product" />
 
           <section class="flex mt-12">
             <AppButton
@@ -94,12 +50,13 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator"
+import { Vue, Component, Prop } from "nuxt-property-decorator"
 
+import TheProductReservation from "~/components/Product/TheProductReservation.vue"
 import { Brightness, Variant } from "~/components/UI/AppButton.vue"
 import { Product } from "~/models"
 
-@Component
+@Component({ components: { TheProductReservation } })
 export default class TheProductHeader extends Vue {
   @Prop({ type: Object, required: true }) readonly product!: Product
 
@@ -108,13 +65,6 @@ export default class TheProductHeader extends Vue {
 
   get blueAngelImageUrl() {
     return require("~/assets/images/products/certificates/blauer-engel-uz120d.svg")
-  }
-
-  get pricePerPackagingUnit() {
-    if (!this.product.basePriceGross || !this.product.packagingUnitContent) return 0
-    const basePrice = +this.product.basePriceGross.replace(",", ".")
-    const packagingUnitContent = +this.product.packagingUnitContent.replace(",", ".")
-    return basePrice * packagingUnitContent
   }
 }
 </script>
--- client/utils/index.ts
+++ client/utils/index.ts
@@ -1,2 +1,4 @@
+export * from "./locale"
 export * from "./location"
 export * from "./math"
+export * from "./validation"
